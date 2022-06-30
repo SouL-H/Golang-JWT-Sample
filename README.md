0eb9bde1ab4e4f85ffde02c8fe0a1aedcf65da25
SouL-H <mhmtaydinw@gmail.com>
2022-06-30 11:24:00 +0300

    readme and img added

---
+++ README.md
@@ -0,0 +1,58 @@
+
+# Golang-JWT-Sample
+
+### Starting (M1-ARM64)
+
+```
+go mod tidy
+```
+
+```
+Server Starting
+
+cd ./server
+go run main.go
+
+```
+
+```
+Client Starting
+cd ./client
+go run main.go
+
+
+```
+
+
+```
+Create Token
+
+GET 
+localhost:8080
+```
+#
+<p>
+    <img src="./img/getToken.png"  style="width:630px;" alt="Observer">
+
+</p>
+
+#
+
+```
+
+Login
+
+localhost:8081
+
+```
+
+#
+
+<p>
+    <img src="./img/login.png"  style="width:630px;" alt="Observer">
+
+</p>
+
+
+
+####    Thank You TutorialEdge
--- client/main.go
+++ client/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
@@ -17,19 +16,19 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println(err)
 	}
-	client := &http.Client{}
-	req, _ := http.NewRequest("GET", "http://localhost:8081/", nil)
-	req.Header.Set("Token", validToken)
-	res, err := client.Do(req)
-	if err != nil {
-		fmt.Fprintf(w, "Error: %s", err.Error())
-	}
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		fmt.Fprintf(w, err.Error())
-	}
+	// client := &http.Client{}
+	// req, _ := http.NewRequest("GET", "http://localhost:8081/", nil)
+	// req.Header.Set("Token", validToken)
+	// res, err := client.Do(req)
+	// if err != nil {
+	// 	fmt.Fprintf(w, "Error: %s", err.Error())
+	// }
+	// body, err := ioutil.ReadAll(res.Body)
+	// if err != nil {
+	// 	fmt.Fprintf(w, err.Error())
+	// }
 
-	fmt.Fprintf(w, string(body))
+	fmt.Fprintf(w, string(validToken))
 }
 func GenerateJWT() (string, error) {
 	token := jwt.New(jwt.SigningMethodHS256)
